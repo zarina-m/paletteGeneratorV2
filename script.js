e1ca3cf4a28562c199ab5cc2dfe18449fb689c37
Zarina M <zarinabliss@gmail.com>
2020-06-22 22:14:20 -0400

    began basic layout

--- script.js
+++ script.js
@@ -19,68 +19,38 @@ app.colour5 = document.querySelector('.colour5');
 app.handleClick = function (e) {
     console.log(e.target.name)
 
+
     if (e.target.name === 'earlGrey') {
-        app.colour1.style.backgroundColor = '#' + app.earlGrey[0];
-        app.colour1.innerText = app.earlGrey[0]
-        app.colour2.style.backgroundColor = '#' + app.earlGrey[1];
-        app.colour2.innerText = app.earlGrey[1]
-        app.colour3.style.backgroundColor = '#' + app.earlGrey[2];
-        app.colour3.innerText = app.earlGrey[2]
-        app.colour4.style.backgroundColor = '#' + app.earlGrey[3];
-        app.colour4.innerText = app.earlGrey[3]
-        app.colour5.style.backgroundColor = '#' + app.earlGrey[4];
-        app.colour5.innerText = app.earlGrey[4]
+        for (let i = 0;i<5; i++ ) {
+            app['colour' + (i + 1)]['style']['backgroundColor'] = '#' + app.earlGrey[i];
+            app['colour' + (i + 1)]['innerText'] = app.earlGrey[i];
+        }
     } else if (e.target.name === 'mangoSorbet') {
-        app.colour1.style.backgroundColor = '#' + app.mangoSorbet[0];
-        app.colour1.innerText = app.mangoSorbet[0]
-        app.colour2.style.backgroundColor = '#' + app.mangoSorbet[1];
-        app.colour2.innerText = app.mangoSorbet[1]
-        app.colour3.style.backgroundColor = '#' + app.mangoSorbet[2];
-        app.colour3.innerText = app.mangoSorbet[2]
-        app.colour4.style.backgroundColor = '#' + app.mangoSorbet[3];
-        app.colour4.innerText = app.mangoSorbet[3]
-        app.colour5.style.backgroundColor = '#' + app.mangoSorbet[4];
-        app.colour5.innerText = app.mangoSorbet[4]
+        
+        for (let i = 0;i<5; i++ ) {
+            app['colour' + (i + 1)]['style']['backgroundColor'] = '#' + app.mangoSorbet[i];
+            app['colour' + (i + 1)]['innerText'] = app.mangoSorbet[i];
+        }
     } else if (e.target.name === 'liquorice') {
-        app.colour1.style.backgroundColor = '#' + app.liquorice[0];
-        app.colour1.innerText = app.liquorice[0]
-        app.colour2.style.backgroundColor = '#' + app.liquorice[1];
-        app.colour2.innerText = app.liquorice[1]
-        app.colour3.style.backgroundColor = '#' + app.liquorice[2];
-        app.colour3.innerText = app.liquorice[2]
-        app.colour4.style.backgroundColor = '#' + app.liquorice[3];
-        app.colour4.innerText = app.liquorice[3]
-        app.colour5.style.backgroundColor = '#' + app.liquorice[4];
+        for (let i = 0; i < 5; i++) {
+            app['colour' + (i + 1)]['style']['backgroundColor'] = '#' + app.liquorice[i];
+            app['colour' + (i + 1)]['innerText'] = app.liquorice[i];
+        }
     } else if (e.target.name === 'cookieCrumble') {
-        app.colour1.style.backgroundColor = '#' + app.cookieCrumble[0];
-        app.colour1.innerText = app.cookieCrumble[0]
-        app.colour2.style.backgroundColor = '#' + app.cookieCrumble[1];
-        app.colour2.innerText = app.cookieCrumble[1]
-        app.colour3.style.backgroundColor = '#' + app.cookieCrumble[2];
-        app.colour3.innerText = app.cookieCrumble[2]
-        app.colour4.style.backgroundColor = '#' + app.cookieCrumble[3];
-        app.colour4.innerText = app.cookieCrumble[3]
-        app.colour5.style.backgroundColor = '#' + app.cookieCrumble[4];
+        for (let i = 0; i < 5; i++) {
+            app['colour' + (i + 1)]['style']['backgroundColor'] = '#' + app.cookieCrumble[i];
+            app['colour' + (i + 1)]['innerText'] = app.cookieCrumble[i];
+        }
     } else if (e.target.name === 'cinnamonHearts') {
-        app.colour1.style.backgroundColor = '#' + app.cinnamonHearts[0];
-        app.colour1.innerText = app.cinnamonHearts[0]
-        app.colour2.style.backgroundColor = '#' + app.cinnamonHearts[1];
-        app.colour2.innerText = app.cinnamonHearts[1]
-        app.colour3.style.backgroundColor = '#' + app.cinnamonHearts[2];
-        app.colour3.innerText = app.cinnamonHearts[2]
-        app.colour4.style.backgroundColor = '#' + app.cinnamonHearts[3];
-        app.colour4.innerText = app.cinnamonHearts[3]
-        app.colour5.style.backgroundColor = '#' + app.cinnamonHearts[4];
+        for (let i = 0; i < 5; i++) {
+            app['colour' + (i + 1)]['style']['backgroundColor'] = '#' + app.cinnamonHearts[i];
+            app['colour' + (i + 1)]['innerText'] = app.cinnamonHearts[i];
+        }
     } else if (e.target.name === 'matcha') {
-        app.colour1.style.backgroundColor = '#' + app.matcha[0];
-        app.colour1.innerText = app.matcha[0]
-        app.colour2.style.backgroundColor = '#' + app.matcha[1];
-        app.colour2.innerText = app.matcha[1]
-        app.colour3.style.backgroundColor = '#' + app.matcha[2];
-        app.colour3.innerText = app.matcha[2]
-        app.colour4.style.backgroundColor = '#' + app.matcha[3];
-        app.colour4.innerText = app.matcha[3]
-        app.colour5.style.backgroundColor = '#' + app.matcha[4];
+        for (let i = 0; i < 5; i++) {
+            app['colour' + (i + 1)]['style']['backgroundColor'] = '#' + app.matcha[i];
+            app['colour' + (i + 1)]['innerText'] = app.matcha[i];
+        }
     }
 }
 
